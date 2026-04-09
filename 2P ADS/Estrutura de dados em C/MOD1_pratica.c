826045d8ac0be622c99810a24e4618f5ca242697
Ramon Nascimento <ramon.dev.rs@gmail.com>
2026-04-08 21:55:51 -0300

    comando struct

--- 2P ADS/Estrutura de dados em C/MOD1_pratica.c
+++ 2P ADS/Estrutura de dados em C/MOD1_pratica.c
@@ -1,1 +1,30 @@
-#include <stdios.h>
+#include <stdio.h>
+#include <stdalign.h>
+#include <string.h>
+
+#define MAX_LIVROS 50
+#define TAM_STRING 100
+
+struct Livros
+{
+    char nome[TAM_STRING];
+    char autor[TAM_STRING];
+    char editora[TAM_STRING];
+    int edicao;
+};
+
+
+void limparbufferEntrada() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+ int main(){
+     struct Livros biblioteca[MAX_LIVROS];
+     int totalLivros = 0;
+     int opcao;
+
+     do {
+         printf("=========================");
+     }
+ }
