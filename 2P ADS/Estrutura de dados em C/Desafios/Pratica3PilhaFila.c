49248041ea9e5e2d970a962a68e5e961a638e5fe
Ramon Nascimento <ramon.dev.rs@gmail.com>
2026-04-18 20:13:46 -0300

    Pilha fila reversão da ordem

---
+++ 2P ADS/Estrutura de dados em C/Desafios/Pratica3PilhaFila.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 50
+
+typedef struct 
+{
+    char nome[30];
+    int idade;
+} Pessoa;
+
+typedef struct 
+{
+    Pessoa dados[MAX];
+    int inicio, fim;
+} Fila;
+
+typedef struct 
+{
+    Pessoa dados[MAX];
+    int topo;
+}Pilha;
+
+void inicializarFila(Fila *f){
+    f->inicio = 0;
+    f->fim = 0;
+}
+
+void inicilizarPilha(Pilha *p){
+    p->topo = - 1;
+}
+
+void inserir(Fila *f, Pessoa p){
+    if (f->fim < MAX){
+        f->dados[f->fim++] = p;
+    }
+}
+
+Pessoa remover (Fila *f){
+    return f->dados[f->inicio++];
+}
+
+void push(Pilha *p, Pessoa pes){
+    if(p->topo < MAX -1) {
+        p->dados[++p->topo] = pes;
+    }
+}
+
+Pessoa pop(Pilha *p){
+    return p->dados[p->topo--];
+}
+
+void exibirFila(Fila f){
+    for (int i = f.inicio; i < f.fim; i++){
+        printf("nome: %s | idade: %d\n", f.dados[i].nome, f.dados[i].idade);
+    }
+}
+
+int main(){
+    Fila filaOriginal, filaInvertida;
+    Pilha auxiliar;
+
+    inicializarFila(&filaOriginal);
+    inicializarFila(&filaInvertida);
+    inicilizarPilha(&auxiliar);
+
+    Pessoa pessoas[] = {
+        {"Carlos", 45}, {"Joana", 62}, {"Lucas", 30}, {"Maria", 75}
+    };
+    for (int i = 0; i < 4; i++){
+        inserir(&filaOriginal, pessoas[i]);
+    }
+
+    printf("Fila original:\n");
+    exibirFila(filaOriginal);
+
+    while (filaOriginal.inicio <filaOriginal.fim)
+    {
+        push(&auxiliar, remover(&filaOriginal));
+    }
+    
+    while (auxiliar.topo >= 0)
+    {
+        inserir(&filaInvertida, pop(&auxiliar));
+    }
+    printf("fila invertida:\n");
+    exibirFila(filaInvertida);
+
+    return 0;
+}
