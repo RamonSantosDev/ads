7f030f57226ccaa3608632057aa0a98af77e5dcb
Ramon Nascimento <ramon.dev.rs@gmail.com>
2026-04-29 20:50:21 -0300

    aula 9

---
+++ 2P ADS/Scripts.python/Aula 09.py
@@ -0,0 +1,27 @@
+# Fatiamento
+print('====Fatiamento====')
+frase = 'Curso em Vídeo Python'
+print(frase)
+print(frase[9])
+print(frase[9:13])
+
+# Analise
+print('===Analise===')
+print(len(frase))
+print(frase.count('o'))
+print(frase.find('deo'))
+print('Curso' in frase)
+
+# Transformação
+print('===Transformação===')
+print(frase.replace('Python', 'Android'))
+print(frase.upper())
+print(frase.lower())
+print(frase.capitalize())
+print(frase.title())
+
+#Divisão
+print('===Divisão===')
+print(frase.split())
+dividido = frase.split()
+print(dividido[0])
