82728e5c6c5c117eddb54188df8f9d4ef6847a2b
Ramon Nascimento <ramon.dev.rs@gmail.com>
2026-05-02 19:47:05 -0300

    desafio 49

---
+++ 2P ADS/Scripts.python/Desafio 48.py
@@ -0,0 +1,9 @@
+soma = 0
+for n in range(3, 501, 3):
+    if n % 2 == 1:
+
+        soma = soma + n
+print ('a soma dos Multiplos de 3 impares é {}'.format(soma))
+
+
+
---
+++ 2P ADS/Scripts.python/Desafio 49.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero: '))
+print('---------Tabuada-----------')
+for c in range(1, 11):
+    resultado = c * n1
+    print('{} x {} = {}'.format(n1 , c, resultado))
