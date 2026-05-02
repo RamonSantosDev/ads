370428f3e1e4cb7409ebe2f145eb27d4b53e33a7
Ramon Nascimento <ramon.dev.rs@gmail.com>
2026-05-02 13:22:17 -0300

    Update Desafio 45

--- 2P ADS/Scripts.python/Desafio 45.py
+++ 2P ADS/Scripts.python/Desafio 45.py
@@ -1,6 +1,7 @@
 # criar um programa que faça o computador jogar Jokenpô com Você.
 
 import random
+from time import sleep
 print('-=-'*10)
 print(' '*8,'Jogo Jokenpô'.upper())
 print('-=-'*10)
@@ -9,6 +10,12 @@ print('[ 1 ] TESOURA')
 print('[ 2 ] PAPEL')
 print('[ 3 ] PEDRA')
 opcao = int(input('Qual o opção: '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+sleep(0.5)
 computador = random.randint(1, 3)
 
 itens = ('Tesoura', 'Papel', 'Pedra')
@@ -16,14 +23,16 @@ itens = ('Tesoura', 'Papel', 'Pedra')
 if opcao < 1 or opcao > 3:
     print('Opção invalida.')
 else:
-    print(f'Você: {itens[opcao-1]}')
-    print(f'Computador: {itens[computador-1]}')
+    print('-=-'*15)
+    print(f'Computador jogou: {itens[computador - 1]}')
+    print(f'Jogador jogou: {itens[opcao-1]}')
+    print('-=-' * 15)
 
 if (opcao == 1 and computador ==  1) or (opcao == 2 and computador == 2) or (opcao == 3 and computador == 3):
-   print('Empate')
+   print('EMPATE')
 elif (opcao == 1 and computador == 2) or (opcao == 2 and computador == 3) or (opcao == 3 and computador == 1):
-    print('Você venceu!')
+    print('JOGADOR VENCEU!')
 elif (opcao == 1 and computador == 3) or (opcao == 2 and computador == 1) or (opcao == 3 and computador == 2):
-    print('Computador Venceu!')
+    print('Computador Venceu!'.upper())
 else:
     print('Opção invalida.')
